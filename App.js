936e27c658456ea9290afd088447683622787b71
watilacosta <watilafreire26@gmail.com>
2019-02-28 17:34:47 -0300

    fix

---
+++ App.js
@@ -0,0 +1,32 @@
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>React Native APP</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      
+    )
+  }
+}
