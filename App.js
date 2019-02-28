0c0bd46ab4063c70692c9d9823e6b0236e29fac3
watilacosta <watilafreire26@gmail.com>
2019-02-28 17:50:44 -0300

    Modificando o componente app

--- App.js
+++ App.js
@@ -20,13 +20,27 @@
 //   },
 // });
 
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default class App extends React.Component {
   render() {
     return (
-      
+      <View style={styles.container}>
+        <Text style={styles.f20}>WÁTILA</Text>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems:     'center',
+  },
+
+  f20: {
+    fontSize: 20
+  }
+})
