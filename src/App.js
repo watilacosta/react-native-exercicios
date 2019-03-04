309b50eeeda93ac666d38782a454652fe6f14b2e
watila <watilafreire26@gmail.com>
2019-03-04 18:46:05 -0300

    Import/Export do react-native

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { View, StyleSheet } from 'react-native'
 
 import Simples from './componentes/Simples'
 import Parimpar from './componentes/Parimpar'
+import Inverter, { MegaSena } from './componentes/Multi'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Simples texto='Texto flexível!'/>
-        <Parimpar numero={31} />
+        <Parimpar numero  = {31} />
+        <Inverter texto   = 'React Native' />
+        <MegaSena numeros = {5} />
       </View>
     )
   }
