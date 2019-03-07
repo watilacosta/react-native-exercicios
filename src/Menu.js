0b2af87393ba56f5e9ed05a3f0bf63abf3586037
watilacosta <watilafreire26@gmail.com>
2019-03-07 15:07:07 -0300

    Atualizando dependencias e mostrando informação de acordo com a plataforma

--- src/Menu.js
+++ src/Menu.js
@@ -5,8 +5,12 @@ import Simples from './componentes/Simples'
 import Parimpar from './componentes/Parimpar'
 import Inverter, { MegaSena } from './componentes/Multi'
 import Contador from './componentes/Contador'
+import Plataformas from './componentes/Plataformas'
 
 export default createDrawerNavigator({
+  Plataformas: {
+    screen: () => <Plataformas />
+  },
   Contador: {
     screen: () => <Contador numeroInicial = {0} />
   },
